731765937b1a4e077f94f13e174a9e63576e8067
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-10-25 17:34:46 -0200

    feat: Adição do componente Footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import logo from '../logo.svg'
 import './App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
@@ -17,6 +18,14 @@ class App extends Component {
               icon='oi oi-eye' />
           </div>
         </header>
+        <footer>
+          <div className='row'>
+            <Footer
+              name='Daniel Souza Amazonas'
+              year='2018'
+              notice='all rights reserved' />
+          </div>
+        </footer>
       </div>
     )
   }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ({ name, year, notice }) => {
+  return (
+    <div
+      className='col-md-12 text-center fixed-bottom text-capitalize'>
+      <footer>
+        <p>© {year} {name} - {notice}</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
