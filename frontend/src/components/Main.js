dcfc039ab5589999de6bb592652342923ed4fee7
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-10-31 16:10:18 -0200

    feat: Adição do componente Main

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import Posts from './Posts'
+
+const Main = () => {
+  return (
+    <div className='col-md-12'>
+      <div className='row'>
+        <Header
+          title='readable'
+          subtitle='react nanodegree'
+          icon='oi oi-eye'
+        />
+      </div>
+      <div className='row'>
+        <Posts local='main' />
+      </div>
+      <div className='row'>
+        <Footer
+          name='daniel souza amazonas'
+          year='2018'
+          notice='all rights reserved'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Main
